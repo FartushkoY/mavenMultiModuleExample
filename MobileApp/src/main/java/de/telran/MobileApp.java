943d823280e586a22ed8fda6994a9d10da61e646
Yevgeniia Fartushko <yevgeshkaf@gmail.com>
2024-06-21 01:23:59 +0200

    add files MobileApp

---
+++ MobileApp/src/main/java/de/telran/MobileApp.java
@@ -0,0 +1,14 @@
+package de.telran;
+
+/**
+ * Hello world!
+ *
+ */
+public class MobileApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! MobileApp" );
+
+    }
+}
---
+++ MobileApp/src/test/java/de/telran/MobileAppTest.java
@@ -0,0 +1,38 @@
+package de.telran;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class MobileAppTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public MobileAppTest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( MobileAppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
